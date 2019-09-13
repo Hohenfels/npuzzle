9f98b101627b00f6fa708f35cf71a3f0324545f6
Sacrimento <sacrimentocorp@gmail.com>
2019-09-13 02:26:11 +0200

    started researchTree

--- classes/Node.py
+++ classes/Node.py
@@ -1,10 +1,15 @@
 class Node():
-    def __init__(self, h, puzzle, parent = None, g = None, size = None):
+    def __init__(self, hFunc, puzzle, parent = None, g = None, size = None):
         self.puzzle = puzzle
         self.parent = parent
         self.g = parent.g + 1 if g else 1
         self.size = size if size else parent.size
-        self.hFunc = h
+        self.hFunc = hFunc
+        self.isSolved = False
+
+    @property
+    def hash(self):
+        return hash(self.puzzle)
 
     @property
     def score(self):
@@ -12,11 +17,34 @@ class Node():
 
     @property
     def solved(self):
-        return self.h == 0
+        return self.isSolved
 
     @property
     def h(self):
-        s = 0
-        for row in range(self.size):
-            for col in range(self.size):
-                s += self.hFunc(self.puzzle[row * self.size + col])
+        sum = 0
+        for col in range(self.size):
+            for row in range(self.size):
+                coords = self.getSnailCoords(self.size, self.puzzle[row][col])
+                sum += self.hFunc(self.puzzle[coords[0]][coords[1]])
+        if sum == 0:
+            self.isSolved = True
+        return sum
+
+    def getEmptyCoords(self):
+        for y in range(self.size):
+            for x in range(self.size):
+                if self.puzzle[y][x] == 0:
+                    return x, y
+                    
+
+    @staticmethod
+    def getSnailCoords(size, value):
+        r = 0
+        span = size
+        while value > span:
+            value -= span
+            r += 1
+            span -= r % 2
+        d, m = divaluemod(r, 4)
+        c = size - 1 - d
+        return [d + value - 1, c, c - value, d][m], [d, d + value, c, c - value][m]
--- researchTree.py
+++ researchTree.py
@@ -1,6 +1,34 @@
 from Classes.Node import Node
 import numpy
 
-def solvePuzzle(puzzle, h):
-    node = Node(puzzle, h)
+def solvePuzzle(puzzle):
+
+    #### TODO: Check if puzzle is solvable
+
+    initialNode = Node(heuristics.manhattan, puzzle, size = 2147483647)
+    bestNode = initialNode
+    seen = [initialNode.hash]
+
+    while not bestNode.solved:
+        nodes = getNodeChildren(bestNode, seen)
+        seen.extend([n.hash for n in nodes])
+
+        if not nodes:
+            print('Something wxent wrong, need to rollback')
+            exit()
+
+        scores = [n.score for n in nodes]
+        bestNode = nodes[scores.index(min(scores))]
+
     return node
+
+def getNodeChildren(parent, seen): ## parent is a Node
+    l = []
+
+    c = parent.getEmptyCoords()
+    puzzle = []
+
+    for move in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+
+
+    return l
