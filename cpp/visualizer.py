ceaee1798b0adf380adc4944629e12e3cf3a34fe
root <root@DESKTOP-CGJMTKO.localdomain>
2019-12-10 18:11:55 +0100

    Added features in visualizer

--- cpp/visualizer.py
+++ cpp/visualizer.py
@@ -1,7 +1,6 @@
 import pygame
 
 windowSize = 900
-slidingSpeed = 30
 
 class Tile():
     def __init__(self, posX, posY, val):
@@ -21,7 +20,7 @@ class Tile():
         self.target = (self.x + dir[0], self.y + dir[1])
         print(self, self.target, dir)
 
-    def move(self):
+    def move(self, slidingSpeed):
         if self.x != self.target[0] or self.y != self.target[1]:
             self.isMoving = True
             self.x = self.x + (self.target[0] - self.x) / slidingSpeed if abs((self.target[0] - self.x) / slidingSpeed) > 0.05 else self.target[0]
@@ -73,7 +72,6 @@ def parse(filename):
                 else:
                     for l in line.split(' '):
                         currState.append(int(l))
-
     return initState, size, moves
 
 def visu(state, moves, puzzleSize):
@@ -84,13 +82,14 @@ def visu(state, moves, puzzleSize):
                 return True
         return False
 
-    window = pygame.display.set_mode((windowSize, windowSize))
+    window = pygame.display.set_mode((windowSize + 250, windowSize))
     pygame.init()
     pygame.display.set_caption('N-Puzzle')
     done = False
     tileSize = windowSize / puzzleSize
     fontSize = 30 + (10 - size) * 10
     moveIdx = -1
+    slidingSpeed = 30
 
     while not done:
         window.fill((0, 0, 0))
@@ -100,19 +99,38 @@ def visu(state, moves, puzzleSize):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     done = True
-                elif event.key == pygame.K_SPACE:
+                elif event.key == pygame.K_RIGHT:
                     if not moving(state) and moveIdx < len(moves):
                         moveIdx += 1
                         state[moves[moveIdx][0]].moveTo((moves[moveIdx][1][0] * tileSize, moves[moveIdx][1][1] * tileSize))
+                elif event.key == pygame.K_LEFT:
+                    if not moving(state) and moveIdx >= 0:
+                        state[moves[moveIdx][0]].moveTo((-moves[moveIdx][1][0] * tileSize, -moves[moveIdx][1][1] * tileSize))
+                        moveIdx -= 1
+                elif event.key == pygame.K_PAGEUP:
+                    slidingSpeed -= 5 if slidingSpeed - 5 > 0 else 0
+                    print(f"Speeding up sliding speed: {slidingSpeed}")
+                elif event.key == pygame.K_PAGEDOWN:
+                    print(f"Slowing down sliding speed: {slidingSpeed}")
+                    slidingSpeed += 5
         for tile in state:
             if tile.val != 0:
-                tile.move()
+                tile.move(slidingSpeed)
                 pygame.draw.rect(window, (255, 127, 80), pygame.Rect(tile.x, tile.y, tileSize - 2, tileSize - 2))
-                window.blit(pygame.font.SysFont(None, fontSize, 1).render(str(tile.val), False, (255, 255, 255)), (tile.x + tileSize / 2 - len(str(tile.val)) * fontSize / 2, tile.y + tileSize / 2 - len(str(tile.val)) * fontSize / 2))
+                window.blit(pygame.font.SysFont(None, 
+                    fontSize, 1).render(str(tile.val),
+                    False,(255, 255, 255)),
+                    (tile.x + tileSize / 2 - len(str(tile.val)) * fontSize / 2,
+                    tile.y + tileSize / 2 - len(str(tile.val)) * fontSize / 2))
         pygame.display.update()
-
     pygame.quit()
 
 if __name__ == "__main__":
     initState, size, moves = parse('path.txt')
     visu(initState, moves, size)
+
+"""
+TODO: Side menu avec path length et legende des controles du visu
+TODO: Plutot qu'espace faire left-right pour next-prev
+
+"""
